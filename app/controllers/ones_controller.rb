3a59e9e4c195a4cd31b6c913f6f2750040756a06
masaru-code <ppurok@yahoo.co.jp>
2021-02-03 18:23:08 +0900

    destroyエラー

--- app/controllers/ones_controller.rb
+++ app/controllers/ones_controller.rb
@@ -14,12 +14,31 @@ class OnesController < ApplicationController
   end
 
   def create
+    @one = One.new(one_params)
+
+    respond_to do |format|
+      if @one.save
+        redirect_to @one, notice: "oneは作られました"
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
   end
-  
+
   def destroy
+    @one.destroy
+    respond_to do |format|
+      redirect_to ones_url, notice: "oneは削除されました"
+    end
   end
 
   def update
+  end
+
+  private
+  
+  def one_params
+    params.require(:one).permit(:title, :body)
   end
 
 end
